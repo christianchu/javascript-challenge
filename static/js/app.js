c8d0a0e0387fa5116ba3b7aa2ea0e34b7b9e65a1
Christian Chu <66145369+christianchu@users.noreply.github.com>
2020-09-20 15:57:33 -0700

    loaded

--- static/js/app.js
+++ static/js/app.js
@@ -2,7 +2,6 @@
 var tableData = data;
 // Select the button
 var button = d3.select("#filter-btn");
-
 var inputField = d3.select("#form-control")
 // Select the form
 var form = d3.select("#form");
@@ -12,8 +11,7 @@ var tbody = d3.select("tbody");
 console.log(tableData);
 
 // Create event handlers 
-button.on("click", handleClick);
-form.on("click", handleClick);
+d3.selectAll("#filter-btn").on("click", handleClick);
 
 // Loop Through `tableData` and console.log each ufo sighting object
 tableData.forEach(function(ufoReport) {
@@ -34,15 +32,11 @@ tableData.forEach(function(ufoReport) {
     });
 });
 
-
 function handleClick() {
-
     // Prevent the page from refreshing
     d3.event.preventDefault();
-  
     // Select the input element and get the raw HTML node
     var inputElement = d3.select("#datetime");
-  
     // Get the value property of the input element
     var inputValue = inputElement.property("value");
   
@@ -50,9 +44,5 @@ function handleClick() {
     console.log(tableData);
 
     var filteredData = tableData.filter(tableData => tableData.datetime === inputValue);
-    console.log(filteredData);
-    // Set the span tag in the h1 element to the text
-    // that was entered in the form
-    // d3.select("h1>span").text(inputValue);
+    buildTable(filteredData);
 };
-  
